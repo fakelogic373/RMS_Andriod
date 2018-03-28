90ab2c6fd72392813edc3ce106b466cb1659fc10
khalid <khalid@khalid.com>
2018-03-28 16:15:34 +0300

    +

--- login.js
+++ login.js
@@ -19,14 +19,6 @@ export default class Login extends React.Component {
         data: []
     }
 
-    
-
-
-
-
-
-
-
     render() {
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
