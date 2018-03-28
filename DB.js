9c9de3312a83790cea71b310ff38c300d9680ec1
khalid <khalid@khalid.com>
2018-03-28 16:40:49 +0300

    +fixed my order

--- DB.js
+++ DB.js
@@ -22,7 +22,7 @@ export default class DB {
                 headers: headers
             });
             var data = await response.json();
-            console.log(data);
+            //console.log(data);
             action(data)
         }
         catch (e) {
--- MyOrder.js
+++ MyOrder.js
@@ -16,73 +16,6 @@ export default class MyOrder extends React.Component {
         };
     };
 
-
-    // state = {
-    //     data: [],
-    //     num: 2
-    // }
-
-    // buy = new DB('http://192.168.56.1:45457/api/User')
-
-    // Quary = (parameters) => {
-    //     this.buy.find(
-    //         (data) => this.setState({}),
-    //         parameters
-    //     )
-    // }
-
-
-
-
-
-    // async componentWillMount() {
-
-    //     return await fetch('http://192.168.56.1:45457/api/User?query=orderitems')
-    //         .then((response) => response.json())
-    //         .then((responseJson) => {
-    //             console.log(responseJson)
-
-    //             this.setState({
-    //                 data: responseJson
-    //             }, function () {
-
-    //             });
-
-    //         })
-    //         .catch((error) => {
-    //             console.error(error);
-    //         });
-    // }
-
-    // handleReload = async (val) => {
-    //     if(val){
-    //         return await fetch('http://192.168.56.1:45457/api/User?query=orderitems')
-    //         .then((response) => response.json())
-    //         .then((responseJson) => {
-    //             console.log(responseJson)
-
-    //             this.setState({
-    //                 data: responseJson
-    //             }, function () {
-
-    //             });
-
-    //         })
-    //         .catch((error) => {
-    //             console.error(error);
-    //         });
-    //     }
-    // }
-
-    // handleCheckout = () =>{
-    //     console.log("im checking out " )
-    //     this.Quary({
-    //         query: "checkout"
-
-    //     })
-  
-    // }
-
     state = {
         order: null,
         OrderItems: [],
@@ -95,26 +28,10 @@ export default class MyOrder extends React.Component {
     componentDidMount() {
         this.findCurrentUser()
         this.findOrderItem()
+        console.log(this.state.OrderItems)
     }
 
-    // find = async (parameters) => {
-    //     await this.db.findOne(
-    //         2,
-    //         (data) => this.setState({ order: data })
-    //     )
-    // }
 
-    // findCurrentUser = async (parameters) => {
-    //     await this.dbUser.find(
-    //         (data) => this.db.findOne(
-    //             data.CustomerId,
-    //             data => this.setState({ order: data })
-    //         ),
-    //         {
-    //             query: "customer"
-    //         }
-    //     )
-    // }
 
     findCurrentUser = async (parameters) => {
         await this.dbUser.find(
@@ -135,7 +52,7 @@ export default class MyOrder extends React.Component {
 
     findOrderItem = async (parameters) => {
         await this.dbUser.find(
-            (data) => this.setState({ data: data }),
+            (data) => this.setState({ OrderItems: data }),
             {
                 query: "orderitems"
             }
@@ -161,25 +78,14 @@ export default class MyOrder extends React.Component {
     }
 
 
-
     render() {
-
-        // const { params } = this.props.navigation.state;
-        // const isReload = params ? params.isReload : null;
-        // const otherParam = params ? params.otherParam : null;
-
-        // this.handleReload(isReload)
-
-        console.log(this.state.data)
-
-        
 
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 <Text> My current order</Text>
                 {/* <Text> number : {this.state.num}</Text> */}
                 <FlatList
-                    data={this.state.data}
+                    data={this.state.OrderItems}
                     keyExtractor={(x, i) => i}
                     renderItem={({ item }) =>
                         <View style={{ flex: 1, flexDirection: 'row' }}>
